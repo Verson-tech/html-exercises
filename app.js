f8b287b0a3470b8cae1ee1b21606967995a4f036
Vira Murray <vira.avramenko@gmail.com>
2022-10-17 14:53:12 -0400

    added .js file

---
+++ app.js
@@ -0,0 +1,24 @@
+//Exercise 1/2: Assigning Values to Variables
+const firstName = "Vera";
+let lastName = "Murray";
+var age = 38;
+
+//Exercise 3: Declaring and Assigning Values to Variable
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+//Exercise 4: Declaring and Assigning Values to Variables x2
+let price = 19.99;
+let isOnSale =false;
+let salePercentage = 15;
+let stock =0;
+let inStock =false;
+let selectedSize  = "M";
+
+//Exercise 5: Declaring and Assigning Values to Variables x3
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
